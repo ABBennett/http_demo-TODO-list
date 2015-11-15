a067d9b8996445284cfdc8e059b17a8ebaa367a5
AlexBennett <alexanderbondbennett@gmail.com>
2015-11-15 17:38:08 -0500

    Added each loop to tasks array

--- server.rb
+++ server.rb
@@ -5,25 +5,30 @@ set :public_folder, File.join(File.dirname(__FILE__), "public")
 get "/hello" do
   "<p>Hello, world! The current time is #{Time.now}.</p>"
 end
+
 get "/tasks" do
- # The three quotes below is simply to treat everything below as a string
- '''
- <!DOCTYPE html>
- <html>
-   <head>
-     <title>Basic HTML Page</title>
-     <link rel="stylesheet" href="home.css">
-   </head>
+  tasks = ["pay bills", "buy milk", "learn Ruby"]
 
-   <body>
-     <h1>TODO list</h1>
+  html = '''
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <title>Basic HTML Page</title>
+      <link rel="stylesheet" href="home.css">
+    </head>
 
-     <ul>
-       <li>pay bills</li>
-       <li>buy milk</li>
-       <li>learn Ruby</li>
-     </ul>
-   </body>
- </html>
- '''
+    <body>
+      <h1>TODO list</h1>
+      <ul>
+  '''
+
+  tasks.each do |task|
+    html += "<li>#{task}</li>"
+  end
+
+  html += '''
+      </ul>
+    </body>
+  </html>
+  '''
 end
